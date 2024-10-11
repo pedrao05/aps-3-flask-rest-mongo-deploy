33e371c06ef3e0b4529d9f062659458f0bfa914f
pedrao05 <pedro.olivieri@icloud.com>
2024-10-11 16:57:30 -0300

    Rotas emprestimos

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo, ObjectId
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv('.cred')
 
@@ -95,8 +96,8 @@ def deletar_bike(id):
 def cadastrar_usuario():
     try:
         data = request.json
-        if not all(key in data for key in ("nome", "cpf", "data de nascimento")):
-            return jsonify({"erro": "Dados incompletos. Os campos 'marca', 'modelo' e 'cidade' são obrigatórios."}), 400
+        if not all(key in data for key in ("nome", "cpf", "data_nascimento")):
+            return jsonify({"erro": "Dados incompletos. Os campos 'nome', 'cpf' e 'data_nascimento' são obrigatórios."}), 400
 
         usuario_id = mongo.db.usuarios.insert_one(data)
         data['_id'] = str(usuario_id.inserted_id)
@@ -167,6 +168,76 @@ def deletar_usuario(id):
     except Exception as e:
         return jsonify({"erro": str(e)}), 500
 
+
+# Inserir um emprestimo dado um id de usuario e id da bicicleta
+@app.route('/emprestimos/usuarios/<id_usuario>/bikes/<id_bike>', methods=['POST'])
+def cadastrar_emprestimo(id_usuario, id_bike):
+    try:
+        bike = mongo.db.bikes.find_one({"_id": ObjectId(id_bike)})
+        if not bike:
+            return jsonify({"erro": "Bicicleta não encontrada"}), 404
+
+        if bike['status'] == 'em uso':
+            return jsonify({"erro": "Bicicleta já está alugada"}), 400
+
+        usuario = mongo.db.usuarios.find_one({"_id": ObjectId(id_usuario)})
+        if not usuario:
+            return jsonify({"erro": "Usuário não encontrado"}), 404
+
+        mongo.db.bikes.update_one({"_id": ObjectId(id_bike)}, {"$set": {"status": "em uso"}})
+
+        data_emprestimo = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        emprestimo_id = mongo.db.emprestimos.insert_one({
+            "id_usuario": ObjectId(id_usuario),
+            "id_bike": ObjectId(id_bike),
+            "data_emprestimo": data_emprestimo,
+            "data_devolucao": None
+        }).inserted_id
+
+        return jsonify({"mensagem": "Empréstimo registrado com sucesso!", "emprestimo_id": str(emprestimo_id)}), 201
+
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+
+
+# Ler todos emprestimos
+@app.route('/emprestimos', methods=['GET'])
+def get_emprestimos():
+    try:
+        emprestimos = list(mongo.db.emprestimos.find())
+
+        resultado = []
+        for emprestimo in emprestimos:
+            resultado.append({
+                "_id": str(emprestimo['_id']),
+                "id_usuario": str(emprestimo['id_usuario']),
+                "id_bike": str(emprestimo['id_bike'])
+            })
+
+        return jsonify(resultado), 200
+    
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+
+
+# Deletar um emprestimo dado um id de empréstimo
+@app.route('/emprestimos/<id>', methods=['DELETE'])
+def deletar_emprestimo(id):
+    try:
+        emprestimo = mongo.db.emprestimos.find_one({"_id": ObjectId(id)})
+        if not emprestimo:
+            return jsonify({"erro": "Empréstimo não encontrado"}), 404
+
+        mongo.db.bikes.update_one(
+            {"_id": ObjectId(emprestimo['id_bike'])},
+            {"$set": {"status": "disponivel"}}
+        )
+
+        mongo.db.emprestimos.delete_one({"_id": ObjectId(id)})
+
+        return jsonify({"mensagem": "Empréstimo deletado com sucesso!"}), 200
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
