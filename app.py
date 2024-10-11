ecb16940a2af755bdc7759a2c821c2a54ab4e383
adventurescumm <gab05mas@gmail.com>
2024-10-11 10:54:40 -0300

    Rotas de usuario

--- app.py
+++ app.py
@@ -90,6 +90,83 @@ def deletar_bike(id):
         return jsonify({"erro": str(e)}), 500
 
 
+# Inserir uma usuario
+@app.route('/usuarios', methods=['POST'])
+def cadastrar_usuario():
+    try:
+        data = request.json
+        if not all(key in data for key in ("nome", "cpf", "data de nascimento")):
+            return jsonify({"erro": "Dados incompletos. Os campos 'marca', 'modelo' e 'cidade' são obrigatórios."}), 400
+
+        usuario_id = mongo.db.usuarios.insert_one(data)
+        data['_id'] = str(usuario_id.inserted_id)
+
+        return jsonify({"mensagem": "Usuario inserido com sucesso!", "usuario": data}), 201
+    
+    except Exception as e:
+        return {"erro":str(e)}, 500
+
+
+# Ler todas as usuarios
+@app.route('/usuarios', methods=['GET'])
+def get_usuarios():
+    try:
+        usuarios = list(mongo.db.usuarios.find())
+        for usuario in usuarios:
+            usuario['_id'] = str(usuario['_id'])
+        return jsonify(usuarios), 200
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+
+
+# Ler apenas uma usuario dado um id
+@app.route('/usuarios/<id>', methods=['GET'])
+def get_usuario(id):
+    try:
+        usuario = mongo.db.usuarios.find_one({"_id": ObjectId(id)})
+        if not usuario:
+            return jsonify({"erro": "Usuario não encontrada"}), 404
+        else:
+            usuario['_id'] = str(usuario['_id'])
+            return jsonify(usuario), 200
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+
+
+# Atualizar uma usuario dado um id
+@app.route('/usuarios/<id>', methods=['PUT'])
+def atualizar_usuario(id):
+    try:
+        data = request.json
+
+        if not data:
+            return jsonify({"erro": "Dados para atualização não fornecidos"}), 400
+
+        usuario = mongo.db.usuarios.find_one({"_id": ObjectId(id)})
+        if usuario:
+
+            mongo.db.usuarios.update_one({"_id": ObjectId(id)}, {"$set": data})
+            return jsonify({"mensagem": "Usuario atualizada com sucesso!"}), 200
+        else:
+            return jsonify({"erro": "Usuario não encontrada"}), 404
+
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+
+
+# Deletar uma usuario dado um id
+@app.route('/usuarios/<id>', methods=['DELETE'])
+def deletar_usuario(id):
+    try:
+        usuario = mongo.db.usuarios.find_one({"_id": ObjectId(id)})
+        if usuario:
+            mongo.db.usuarios.delete_one({"_id": ObjectId(id)})
+            return jsonify({"mensagem": "usuario deletada com sucesso!"}), 200
+        else:
+            return jsonify({"erro": "usuario não encontrada"}), 404
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
